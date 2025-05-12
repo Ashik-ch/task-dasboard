17b82f45a3ac95f2518735e48ea80faf01d4149b
VAASITS <dev@vaasits.com>
2025-05-13 04:43:33 +0530

    chore: update dependencies and add Tailwind CSS configuration
    - Added autoprefixer, postcss, and tailwindcss to package.json for styling enhancements.
    - Created a new Tailwind CSS configuration file (tailwind.config.js) for project styling.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { TaskManagerComponent } from './component/task-manager';
 
 export const routes: Routes = [
-    { path: "/", component: TaskManagerComponent }
+    { path: '', component: TaskManagerComponent }
 ];
