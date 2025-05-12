3cb7d3ccee51055849870d0032e5880bea24eff7
VAASITS <dev@vaasits.com>
2025-05-13 04:30:36 +0530

    Add TaskManagerComponent and set up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TaskManagerComponent } from './component/task-manager';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "/", component: TaskManagerComponent }
+];
