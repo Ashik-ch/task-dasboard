245364ad3ac42dcf11335c901a297d1af2477daa
VAASITS <dev@vaasits.com>
2025-05-13 13:51:10 +0530

    feat: update Angular compiler version and implement Task interface in TaskManagerComponent

--- src/app/component/task-manager/task-manager.component.ts
+++ src/app/component/task-manager/task-manager.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Task } from './helper';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task-manager',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './task-manager.component.html',
   styleUrl: './task-manager.component.css'
 })
 export class TaskManagerComponent {
+  todo: Task[] = [];
+  inProgress: Task[] = [];
+  done: Task[] = [];
+
+
+  ngOnInit(): void {
+  }
 
 }
