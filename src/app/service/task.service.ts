96fae5aeca7a6629e3ababca2346969f29514f03
VAASITS <dev@vaasits.com>
2025-05-14 14:06:45 +0530

    feat: implement TaskService with CRUD operations and update index exports

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Task } from '../component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private API_URL = 'http://localhost:3000/tasks';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.API_URL);
+  }
+
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.API_URL, task);
+  }
+
+  updateTask(task: Task): Observable<Task> {
+    return this.http.put<Task>(`${this.API_URL}/${task.id}`, task);
+  }
 }
+
