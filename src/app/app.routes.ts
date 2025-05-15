5cd43012b4ee5fae919c6de22bbf05d30ad4b210
VAASITS <dev@vaasits.com>
2025-05-15 15:34:06 +0530

    fix: update import path for TaskManagerComponent in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { TaskManagerComponent } from './component/task-manager';
+import { TaskManagerComponent } from './component';
 
 export const routes: Routes = [
     { path: '', component: TaskManagerComponent }
